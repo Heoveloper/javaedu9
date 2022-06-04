1bd1da3d18fa810fd9c027451123f19a99811a65
Heoveloper <wnsgur5508@gmail.com>
2022-06-04 20:58:46 +0900

    220510~

---
+++ day16/src/com/kh/dictionary2/DictionaryException.java
@@ -0,0 +1,9 @@
+package com.kh.dictionary2;
+
+public class DictionaryException extends RuntimeException {
+
+    //ctrl + O > RuntimeException
+    public DictionaryException(String message) {
+        super(message);
+    }
+}
