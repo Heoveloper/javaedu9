fe493ef6f0e1198cb099a4182e3e87a4fee602f0
Junhyeok Heo <106042161+Heoveloper@users.noreply.github.com>
2022-08-05 15:55:32 +0900

    Expo and Spring

---
+++ demo/src/main/java/com/kh/demo/dao/Product.java
@@ -0,0 +1,40 @@
+package com.kh.demo.dao;
+
+public class Product {
+    private Long productId;
+    private String name;
+    private Long quantity;
+    private Long price;
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Long quantity) {
+        this.quantity = quantity;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+}
