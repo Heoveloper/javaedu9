a6ac7448e8b7e4f9aadeefdb99d058d3067afe30
Junhyeok Heo <106042161+Heoveloper@users.noreply.github.com>
2022-08-17 15:27:55 +0900

    Ajax

--- ajax/src/main/java/com/kh/ajax/web/ApiItemController.java
+++ ajax/src/main/java/com/kh/ajax/web/ApiItemController.java
@@ -7,7 +7,6 @@ import com.kh.ajax.web.api.ApiResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.thymeleaf.util.StringUtils;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
