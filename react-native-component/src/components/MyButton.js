6540985dd3feef14d0a65da1957799f658261f8b
Junhyeok Heo <106042161+Heoveloper@users.noreply.github.com>
2022-08-19 12:21:12 +0900

    custom components

--- react-native-component/src/components/MyButton.js
+++ react-native-component/src/components/MyButton.js
@@ -3,8 +3,16 @@ import { TouchableOpacity, Text } from 'react-native';
 
 const MyButton = () => {
     return (
-        <TouchableOpacity>
-            <Text style={{fontSize: 24}}>My Button</Text>
+        <TouchableOpacity
+        style={{
+            backgroundColor: 'skyblue',
+            padding: 16,
+            margin: 10,
+            borderRadius: 8,
+        }}
+        onPress={() => alert('클~~~~~~~~~~~~~~~~~~~~~~~~~릭')}
+        >
+            <Text style={{color: 'white', fontSize: 24, fontWeight: 900}}>My Button</Text>
         </TouchableOpacity>
     );
 };
